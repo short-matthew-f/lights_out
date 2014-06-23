6500b8d8eb63415c89f1309d11af231895fab8e3
Candace Schenk and Matthew Short <short.matthew.f@gmail.com>
2014-06-23 09:23:59 -0400

    adds flair

--- lightsout.js
+++ lightsout.js
@@ -1,11 +1,15 @@
 (function (root) {
   var Game = root.Game = (root.Game || {});
 
-  var LightsOut = Game.LightsOut = function () {
-    this.DELTAS = [ [1, 0], [0, 1], [-1, 0], [0, -1] ];
+  var LightsOut = Game.LightsOut = function (board) {
+    this.DELTAS = [ [1, 0], [0, 1], [-1, 0], [0, -1], [0, 0] ];
 
-    this.board = LightsOut.createBoard(); 
-    this.randomizeBoard();
+    if(board) {
+      this.board = board;
+    } else {
+      this.board = LightsOut.createBoard(); 
+      this.randomizeBoard();
+    };
   }
 
   _(LightsOut).extend({
@@ -30,7 +34,9 @@
     },
 
     randomPosition: function() {
-      return [Math.floor(Math.random()*5), Math.floor(Math.random()*5)];
+      return [
+        Math.floor(Math.random()*5), Math.floor(Math.random()*5)
+      ];
     },
 
 
@@ -39,8 +45,6 @@
   _(LightsOut.prototype).extend({
     toggle: function(x, y) {
       var board = this.board;
-
-      board[x][y] = !board[x][y];
 
       _(this.DELTAS).each(function(direction) {
         var newX = x + direction[0];
@@ -86,30 +90,62 @@
     this.totalClicks = 0;
 
     this.render();
-
-    var that = this;
-
-    $boardEl.click('li', function(event) {
-      that.totalClicks += 1;
-
-      $(".clicks").text("Total clicks: " + that.totalClicks);
-
-      var $t =$(event.target);
-
-      var x = $t.data('row');
-      var y = $t.data('col');
-
-      that.lightsOut.toggle(x,y);
-      that.render();
-
-      if(that.lightsOut.isWon()) {
-        $('.messages').text("You've won!")
-        $boardEl.off('click');
-      }
-    });
+    // this.renderDelta();
+    this.bindClicks();
+    this.bindHovers();
   };
 
   _(LightsOutUI.prototype).extend({
+    bindClicks: function() {
+      var that = this;
+
+      this.$boardEl.click('li', function(event) {
+        that.totalClicks += 1;
+
+        $(".clicks").text("Total clicks: " + that.totalClicks);
+
+        var $t =$(event.target);
+
+        var x = $t.data('row');
+        var y = $t.data('col');
+
+        that.lightsOut.toggle(x,y);
+        that.render();
+        that.bindHovers();
+
+        if(that.lightsOut.isWon()) {
+          $('.messages').text("You've won!")
+          this.$boardEl.off('click');
+        }
+      });
+    },
+
+    bindHovers: function() {
+      var that = this;
+
+      // $(thing).hover('additional selectors', enterFn, leaveFn)
+
+      $('.board li').hover(function(event) {
+        var $t =$(event.target);
+
+        var x = $t.data('row');
+        var y = $t.data('col');
+
+        _(that.lightsOut.DELTAS).each(function(DELTA) {
+          var _x = x + DELTA[0];
+          var _y = y + DELTA[1];
+
+          if(Game.LightsOut.isOnBoard(_x,_y)) {
+            $("li[data-row='" + _x + "'][data-col='" + _y + "']").addClass('hovered');
+          };
+        });
+
+        $t.addClass('hovered');
+      }, function(event) {
+        $('li').removeClass('hovered');
+      });
+    },
+
     render: function() {
       var $ul = $('<ul class="group">');
 
@@ -130,5 +166,23 @@
 
       this.$boardEl.html($ul);
     }
+    // },
+
+    // renderDelta: function() {
+    //   var $ul = $('<ul class="group">');
+
+    //   for(var i = -1; i < 1; i++) {
+    //     for(var j = -1; j < 1; j++) {
+    //       var li = '<li data-row="' + i + '" data-col="' + j + '">';
+
+    //       var $li = $(li);
+    //       $ul.append($li);
+    //     }        
+    //   };
+
+    //   $('.delta').html($ul); 
+    // }
   });
 })(window);
+
+
